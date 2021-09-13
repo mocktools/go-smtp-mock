33266e53a1fc769adc545cd0b99ccd194cb9e30b
Vladislav Trotsenko <admin@bestweb.com.ua>
2021-09-13 10:09:42 +0300

    Technical/Refactor session namespace

--- session.go
+++ session.go
@@ -5,7 +5,7 @@ import (
 	"net"
 )
 
-// smtpSession interfaces
+// session interfaces
 
 type bufin interface {
 	ReadString(byte) (string, error)
@@ -17,7 +17,7 @@ type bufout interface {
 }
 
 // SMTP client-server session
-type smtpSession struct {
+type session struct {
 	connection                             net.Conn
 	address                                string
 	bufin                                  bufin
@@ -25,9 +25,9 @@ type smtpSession struct {
 	readError, writeError, connectionError error
 }
 
-// smtpSession builder
-func newSmtpSession(connection net.Conn) *smtpSession {
-	return &smtpSession{
+// SMTP session builder
+func newSession(connection net.Conn) *session {
+	return &session{
 		connection: connection,
 		address:    connection.RemoteAddr().String(),
 		bufin:      bufio.NewReader(connection),
@@ -35,10 +35,10 @@ func newSmtpSession(connection net.Conn) *smtpSession {
 	}
 }
 
-// smtpSession methods
+// SMTP session methods
 
 // Reades client request from the session. When error case happened writes it to readError
-func (session *smtpSession) readRequest() string {
+func (session *session) readRequest() string {
 	request, err := session.bufin.ReadString('\n')
 	if err != nil {
 		session.readError = err
@@ -48,7 +48,7 @@ func (session *smtpSession) readRequest() string {
 }
 
 // Writes server response to the client session. When error case happened writes it to writeError
-func (session *smtpSession) writeResponse(response string) {
+func (session *session) writeResponse(response string) {
 	bufout := session.bufout
 	_, err := bufout.WriteString(response + "\r\n")
 	if err != nil {
@@ -58,7 +58,7 @@ func (session *smtpSession) writeResponse(response string) {
 }
 
 // Finishes SMTP session. When error case happened writes it to connectionError
-func (session *smtpSession) finish() {
+func (session *session) finish() {
 	err := session.connection.Close()
 	if err != nil {
 		session.connectionError = err
