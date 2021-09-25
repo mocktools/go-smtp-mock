ec40c884012d62838171d49cbe1cd268ea3bfee9
Vladislav Trotsenko <admin@bestweb.com.ua>
2021-09-25 12:26:21 +0300

    Technical/Implement concurrent messages slice

--- message.go
+++ message.go
@@ -1,10 +1,28 @@
 package smtpmock
 
+import "sync"
+
 // Structure for storing the result of SMTP client-server interaction
 type message struct {
 	heloRequest, heloResponse string
 	helo                      bool
 	// mailfromRequest, mailfromResponse string
 	// rcpttoRequest, rcpttoResponse     string
-	// mailfrom, rcptto bool
+	// mailfrom, rcptto            bool
+}
+
+// Concurrent type that can be safely shared between goroutines
+type messages struct {
+	sync.RWMutex
+	items []*message
+}
+
+// messages methods
+
+// Addes new message pointer into concurrent messages slice
+func (messages *messages) append(item *message) {
+	messages.Lock()
+	defer messages.Unlock()
+
+	messages.items = append(messages.items, item)
 }
