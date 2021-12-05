27896442151f80ad40f2ac95af603c28f11b112a
Vladislav Trotsenko <admin@bestweb.com.ua>
2021-12-05 17:36:53 +0200

    Technical/Refactor ConfigurationAttr#assignDefaultValues method (#44)

--- smtpmock.go
+++ smtpmock.go
@@ -1,6 +1,6 @@
 package smtpmock
 
-// New builds new SMTP mock server, based on passed configuration attributes
+// New builds new SMTP mock server based on passed configuration attributes
 func New(config ConfigurationAttr) *Server {
 	return newServer(newConfiguration(config))
 }
