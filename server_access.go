3a5dc01e7d71130f4bf4c080c44b37ac8bd87153
Abdelaziz Elrashed <aeemh.sdn@gmail.com>
2022-04-24 18:21:37 +0300

    Add Method to clear messages and enhance Message access method to cover all cases

--- server_access.go
+++ server_access.go
@@ -6,9 +6,14 @@ func (server *Server) Count() int {
 	return len(server.messages.items)
 }
 
-func (server *Server) Message(index int) string {
-	if server.Count() < index {
-		return ""
+func (server *Server) Message(index uint) string {
+	if int(index) < server.Count() {
+		return repr.String(server.messages.items[index])
 	}
-	return repr.String(server.messages.items[index])
+
+	return ""
+}
+
+func (server *Server) Clear() {
+	server.messages.items = nil
 }
